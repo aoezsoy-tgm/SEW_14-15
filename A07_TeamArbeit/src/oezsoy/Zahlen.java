0636ea3f9b04219d949d557cc3fe2e0e028847fb
Ahmet Oezsoy <aoezsoy@mail.tgm.ac.at>
2014-12-05 10:53:10 +0100

    Zahlenklasse erstellt

--- A07_TeamArbeit/src/oezsoy/Zahlen.java
+++ A07_TeamArbeit/src/oezsoy/Zahlen.java
@@ -1,5 +1,11 @@
 package oezsoy;
 
+/**
+ * Eine Klasse zur Erstellung eines Objektes und Berechnung von Zahlen (Summe, Maximum und Minimum) 
+ * und mittels add-Methode können beliebige Zahlen hinzugefügt werden
+ * @author Oezsoy Ahmet
+ * @version 05.12.2014
+ */
 public class Zahlen {
-
+	
 }
